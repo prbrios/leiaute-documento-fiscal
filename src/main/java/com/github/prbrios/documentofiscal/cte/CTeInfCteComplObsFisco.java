2401ec4c7e193ccb16f4444b54587dd84cf1b6ab
Paulo Rios <prbrios@gmail.com>
2020-01-22 09:57:10 -0300

    Adicionado atributo xCampo

--- src/main/java/com/github/prbrios/documentofiscal/cte/CTeInfCteComplObsFisco.java
+++ src/main/java/com/github/prbrios/documentofiscal/cte/CTeInfCteComplObsFisco.java
@@ -1,12 +1,16 @@
 package com.github.prbrios.documentofiscal.cte;
 
 import lombok.Data;
+import org.simpleframework.xml.Attribute;
 import org.simpleframework.xml.Element;
 import org.simpleframework.xml.Root;
 
 @Data
 @Root(name = "ObsFisco")
 public class CTeInfCteComplObsFisco {
+
+	@Attribute(name = "xCampo", required = false)
+	private String xCampo;
 
 	@Element(name = "xTexto", required = false)
 	private String xTexto;
